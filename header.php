a090bf27273d166c2032d39f5134714dd541c56d
Michael Humphrey <michaelht17@gmail.com>
2019-11-09 14:07:28 -0600

    first commit

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bee Safe</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/styleIndex.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> 
+    <link rel="shortcut icon" href="asset/img/logos/completed-task.ico" type="image/x-icon">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="contenedor">
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Bee Safe</a>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="navbarCollapse" style="">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Guia de prevención</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link disabled" href="#">Nosotros</a>
+            </li>
+        </ul>
+        <form class="form-inline mt-2 mt-md-0">
+            <button class="btn btn-outline-success my-2 my-sm-0 " type="submit">Loging</button>
+        </form>
+        </div>
+    </nav>  
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include('header.php');?>
+    <div class="banner1" id="bannerIndex1"></div>
+    <div class="contTexto">
+        <h1 class="titulosBanner" id="tituloBanner1">Bee Safe</h1>
+        <p class="parrafos cuerpoTexto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente obcaecati voluptatibus inventore animi ipsum placeat, alias dolorum fuga impedit. Assumenda sunt exercitationem corrupti cumque voluptas. Quis, repudiandae harum. Nemo, tempore.</p>
+    </div>
+    <div class="banner1" id="bannerIndex2"></div>
+
+<?php include('footer.php');?>
