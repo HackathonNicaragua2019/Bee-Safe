c3c706648859e2fb8bde6ba44ae69a95968df036
Michael Humphrey <michaelht17@gmail.com>
2019-11-09 22:12:05 -0600

    feat(DB): Creacion de modulo de inserciones en usuarios

--- register.php
+++ register.php
@@ -1,13 +1,13 @@
 <?php include('header.php'); ?>
-    <div class="contenedor">
-        <form action="guardarUsuarios.php" method="POST">
+    <div class="contenedor" id="contenedorRegistro">
+        <form action="guardarUsuarios.php" method="POST" class="forularioRegistro">
             <input type="text" name="nombreUsuario" class="textNombre" id="TxtNombre" placeholder="Nombre">
             <input type="text" name="passwordUsuario" class="textNombre" id="TxtNombre" placeholder="Contraseña">
-            <input type="text" name="cedulaUsuario" class="textNombre" id="TxtNombre" placeholder="Cedula">
+            <input type="text" name="cedulaUsuario" class="textNombre" id="TxtNombre" placeholder="Cedula:161XXXX000XN">
             <input type="hidden" name="distritoUsuario" value="1">
-            <textarea name="descripcionUsuario" id="" cols="30" rows="10" placeholder="Descripcion de Usuario no mas de 250 caracteres"></textarea>
+            <textarea name="descripcionUsuario" id="" cols="30" rows="10" placeholder="Descripcion de Usuario no mas de 250 caracteres" class="textNombre" id="areTexto1"></textarea>
             <input type="hidden" name="tipoUsuario" value="3">
-            <input type="submit" value="Guardar">
+            <input type="submit" value="Registrar" class="inputButton">
         </form>
     </div>
 <?php include('footer.php'); ?>
